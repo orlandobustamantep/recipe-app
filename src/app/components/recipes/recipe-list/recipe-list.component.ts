7423b5086509e36dd008f9e4c5b6b44e1c1553fa
Luis Orlando Bustamante <orlandobustamantep@gmail.com>
2020-02-12 22:06:10 -0700

    select recipe with service

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter, OnInit } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { Recipe } from '../recipe.model'
 import { RecipeService } from '../recipe.service'
 
@@ -9,7 +9,6 @@ import { RecipeService } from '../recipe.service'
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[];
-  @Output() recipeSelected = new EventEmitter<Recipe>()
 
   constructor(private recipeService: RecipeService) {}
 
