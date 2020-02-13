833e63221b1c7174689b1b6d5fc4c15db180c944
Luis Orlando Bustamante <orlandobustamantep@gmail.com>
2020-02-12 19:56:56 -0700

    import shopping list from new location

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { RecipeListComponent } from './components/recipes/recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './components/recipes/recipe-list/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './components/recipes/recipe-detail/recipe-detail.component';
-import { ShoppingListComponent } from './components/shopping-list/shopping-list/shopping-list.component';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
 import { ShoppingListEditComponent } from './components/shopping-list/shopping-list-edit/shopping-list-edit.component';
 import { HeaderComponent } from './components/common/header/header.component';
 import { RecipesComponent } from './components/recipes/recipes.component';
