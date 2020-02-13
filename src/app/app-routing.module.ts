5e20d2991f98251398219a0d029233c61ce4bcf2
Luis Orlando Bustamante <orlandobustamantep@gmail.com>
2020-02-13 13:15:35 -0700

    add routing module for main components

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from "@angular/core"
+import { Routes, RouterModule } from '@angular/router'
+import { RecipesComponent } from './components/recipes/recipes.component'
+import { ShoppingListComponent } from './components/shopping-list/shopping-list.component'
+
+const appRoutes: Routes = [
+    { path: '', redirectTo: '/recipes', pathMatch: 'full'},
+    { path: 'recipes', component: RecipesComponent },
+    { path: 'shopping-list', component: ShoppingListComponent }
+]
+
+@NgModule({
+    imports: [RouterModule.forRoot(appRoutes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
